bbcddbb69f2866148bbe9e84b6f9efb38d1d21b0
Rahul <rahul98112005@gmail.com>
2025-09-18 13:43:27 +0530

    Integrate Coingecko api and display data on home screen

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,64 @@
-import React from 'react'
-import "./Navbar.css"
-import logo from "../../assets/logo.png"
-import arrow_icon from "../../assets/arrow_icon.png"
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import arrow_icon from "../../assets/arrow_icon.png";
+import { useContext } from "react";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={logo} alt="" className='logo' />
-        <ul>
-            <li>Home</li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Blog</li>
-        </ul>
-        <div className="nav-right">
-            <select>
-                <option value="usd">USD</option>
-                <option value="eur">EUR</option>
-                <option value="inr">INR</option>
-            </select>
-            <button>Sign up <img src={arrow_icon} alt="" /></button>
-        </div>
-    </div>
-  )
-}
+  const { setCurrency } = useContext(CoinContext);
 
-export default Navbar
+  const currencyHandler = (e) => {
+    switch (event.target.value) {
+      case "usd":
+        setCurrency({
+          name: "usd",
+          symbol: "$",
+        });
+        break;
+      case "eur":
+        setCurrency({
+          name: "eur",
+          symbol: "€",
+        });
+        break;
+      case "inr":
+        setCurrency({
+          name: "inr",
+          symbol: "Rs",
+        });
+        break;
+      default: {
+        setCurrency({
+          name: "usd",
+          symbol: "$",
+        });
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select onChange={currencyHandler}>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
+        </select>
+        <button>
+          Sign up <img src={arrow_icon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Home.css"
+import { useContext } from 'react'
+import { CoinContext } from '../../context/CoinContext'
+import { useState } from 'react'
 
 const Home = () => {
+
+  const {allCoin, currency} = useContext(CoinContext);
+  const[displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
+  console.log(displayCoin);
+
   return (
     <div className='home'>
       <div className="hero">
@@ -20,6 +33,24 @@ const Home = () => {
             <p style={{textAlign:"center"}}>24H Change</p>
             <p className='market-cap'>Market Cap</p>
         </div>
+        {
+          displayCoin.slice(0,10).map((item,index) => (
+            <div className="table-layout" key={index}>
+              <p>{item.market_cap_rank}</p>
+              <div className="coin-name">
+                <img src={item.image} alt="" />
+                <div>
+                  <p>{item.name + " - " + item.symbol}</p>
+                  <p style={{textTransform:"uppercase", color:"gray"}}>{item.symbol}</p>
+                </div>
+              </div>
+              <p>{currency.symbol}{item.current_price}
+              </p>
+              <p>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
+              <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
